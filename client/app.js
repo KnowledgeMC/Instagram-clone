2337cf40565c962a80bce728c19b265e7a056ba5
Roy Hwang <tupaclypse@gmail.com>
2016-12-14 12:01:29 -0800

    add views, add satellizer.js

--- client/app.js
+++ client/app.js
@@ -1,5 +1,5 @@
-angular.module('Instagram', ['ngRoute', 'ngMessages'])
-  .config(function($routeProvider) {
+angular.module('Instagram', ['ngRoute', 'ngMessages', 'satellizer'])
+  .config(function($routeProvider, $authProvider) {
     $routeProvider
       .when('/',{
         templateUrl: 'views/home.html',
@@ -13,9 +13,22 @@ angular.module('Instagram', ['ngRoute', 'ngMessages'])
         templateUrl: 'views/signup.html',
         controller: 'SignupCtrl'
       })
-      .when('/photo/:id',{
+      .when('/photo/:id', {
         templateUrl: 'views/detail.html',
         controller: 'DetailCtrl'
       })
       .otherwise('/');
+
+    $authProvider.loginUrl = 'http://localhost:3000/auth/login';
+    $authProvider.signupUrl = 'http://localhost:3000/auth/signup';
+    $authProvider.oauth2({
+      name: 'instagram',
+      url: 'http://localhost:3000/auth/instagram',
+      redirectUri: 'http://localhost:8000/',
+      clientId: '16535d4b34a34117834b8a19704e0ad7',
+      requiredUrlParams: ['scope'],
+      scope: ['likes'],
+      scopeDelimiter: '+',
+      authorizatioinEndpoint: 'https://api.instagram.com/oauth/authorize'
+    });
   });
---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: 'localhost',
+  clientSecret: process.env.clientSecret || '16535d4b34a34117834b8a19704e0ad7',
+  tockenSecret: process.env.tokenSecret ||  '61E6394276994C9A21CD52D24D1EA'
+};
